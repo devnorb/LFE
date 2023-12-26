1fb07beaa9e690d26410ec1b02f7ab67a6a7991f
norb_ <127006485+devnorb@users.noreply.github.com>
2023-12-25 20:44:27 -0800

    Update scriptloader.lua

--- scriptloader.lua
+++ scriptloader.lua
@@ -1,3 +1,3 @@
 --[[ LFE UPDATED - norb on top ]]--
 
-loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/1167250916123082752/1189057805932568709/LFE.norbyscripts.lua"))()
+loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/1167250916123082752/1189066087908773888/LFE.norbyscripts.lua"))()
