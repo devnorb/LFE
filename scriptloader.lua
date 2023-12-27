d528f63c09af0348a63b26eb30daae33df46b8d3
norb_ <127006485+devnorb@users.noreply.github.com>
2023-12-26 17:27:25 -0800

    Update scriptloader.lua

--- scriptloader.lua
+++ scriptloader.lua
@@ -1,3 +1,2 @@
---[[ LFE UPDATED - norb on top ]]--
-print("LFE is currently waiting on a fix")
--- loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/1167250916123082752/1189248639604895744/LFE.norbyscripts.lua"))()
+--[[ LFE finally FIXED :D - norb on top ]]--
+loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/1167250916123082752/1189378828649373786/LFEFINAL.norbyscripts"))()
