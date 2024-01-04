5a24cf80413c43a68d7c861520e0010de97d2de8
norb_ <127006485+devnorb@users.noreply.github.com>
2024-01-03 21:16:34 -0800

    Update scriptloader.lua

--- scriptloader.lua
+++ scriptloader.lua
@@ -1,2 +1,2 @@
 --[[ LFE updated adonis bypass - thx to ludi ]]--
-loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/1167250916123082752/1192334923487514655/LFE.norbyscripts"))()
+loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/1167250916123082752/1192335649362165870/LFE.norbyscripts"))()
