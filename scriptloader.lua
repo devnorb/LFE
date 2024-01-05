58a948041b706642fb79355774d9f1b9b5b1f743
norb_ <127006485+devnorb@users.noreply.github.com>
2024-01-05 14:01:21 -0800

    Update scriptloader.lua

--- scriptloader.lua
+++ scriptloader.lua
@@ -1,2 +1,2 @@
---[[ LFE updated adonis bypass - thx to ludi ]]--
-loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/1167250916123082752/1192336076426199071/LFE.norbyscripts"))()
+--[[ LFE: FIXED VIM ]]--
+loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/1167250916123082752/1192950769125699594/LFE.norbyscripts"))()
