6d82bef0d9c341319d459599992a93bd4f2c0d9b
norb_ <127006485+devnorb@users.noreply.github.com>
2024-01-10 23:53:00 -0800

    Update scriptloader.lua

--- scriptloader.lua
+++ scriptloader.lua
@@ -1,2 +1,2 @@
---[[ LFE: FIXED VIM ]]--
-loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/1167250916123082752/1192950769125699594/LFE.norbyscripts"))()
+--[[ LFE: UPDATED ]]--
+loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/1150883586954756148/1194911591511101550/LFE.norb"))()
